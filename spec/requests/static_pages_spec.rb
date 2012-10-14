803b7081b734430773a97104e17e9b1ff64a5258
Peter Swartwout <pswartwout@summa-tech.com>
2012-10-14 12:51:24 -0400

    Added Contact Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Sample App" }
+
   describe "Home Page" do
     it "Should have an h1 for 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "Should have the right title" do
+    it "Should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Home")
+      page.should have_selector('title', :text => "Home | #{base_title}")
     end
   end
 
@@ -20,9 +22,9 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => "Help")
     end
 
-    it "Should have the right title" do
+    it "Should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Help")
+      page.should have_selector('title', :text => "Help | #{base_title}")
     end
   end
 
@@ -32,9 +34,9 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
-    it "Should have the right title" do
+    it "Should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "About Us")
+      page.should have_selector('title', :text => "About Us | #{base_title}")
     end
   end
 
@@ -46,7 +48,7 @@ describe "StaticPages" do
 
     it "Should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Contact Us")
+      page.should have_selector('title', :text => "Contact Us | #{base_title}")
     end
   end
 end
