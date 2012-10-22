439da24a6779c6645298616701089a523df3c187
Peter Swartwout <pswartwout@summa-tech.com>
2012-10-22 18:58:30 -0400

    Hooked up links with named routes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,48 +6,53 @@ describe "StaticPages" do
 
   describe "Home Page" do
     it "Should have an h1 for 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', text: 'Sample App')
     end
 
-    it "Should have the title 'Home'" do
-      visit '/static_pages/home'
+    it "Should have the base title" do
+      visit root_path
       page.should have_selector('title', text: "#{base_title}")
+    end
+
+    it "Should not have the title 'Home'" do
+      visit root_path
+      page.should_not have_selector('title', text: "| Home")
     end
   end
 
   describe "Help Page" do
     it "Should have an H1 for 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', text: "Help")
     end
 
     it "Should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
 
   describe "About Us Page" do
     it "Should have an H1 for 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', text: 'About Us')
     end
 
     it "Should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', text: "#{base_title} | About Us")
     end
   end
 
   describe "Contact Us Page" do
     it "Should have an H1 for 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact Us')
     end
 
     it "Should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', text: "#{base_title} | Contact Us")
     end
   end
